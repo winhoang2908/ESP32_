3b1408a761e112e538ba949c567626169a313837
winhoang2908 <winhoang2908@gmail.com>
2026-03-16 14:39:35 +0700

    add hello v2

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -6,7 +6,7 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-//hello cả nhà 
+//hello cả nhà yêu dấu
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
